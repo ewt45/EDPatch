b1c8d13c62c1917f65f819d96ddba45ca6d3071a
ewt45 <79033456+ewt45@users.noreply.github.com>
2023-04-16 17:53:17 +0800

    测试分支上传

--- app/src/main/java/com/eltechs/axs/xserver/impl/EventsInjectorImpl.java
+++ app/src/main/java/com/eltechs/axs/xserver/impl/EventsInjectorImpl.java
@@ -3,7 +3,6 @@ package com.eltechs.axs.xserver.impl;
 import android.util.Log;
 
 import com.eltechs.axs.xserver.EventsInjector;
-import com.eltechs.axs.xserver.RealXServer;
 import com.eltechs.axs.xserver.XServer;
 
 public class EventsInjectorImpl implements EventsInjector {
@@ -14,35 +13,30 @@ public class EventsInjectorImpl implements EventsInjector {
         this.xServer = xServer;
     }
 
-    public void injectKeyPress(byte keyCode, int keySym) {
-        if (keyCode != 0 && keySym == 0)
-            RealXServer.key(keyCode,1);
-        else if (keyCode == 0 && keySym != 0)
-            RealXServer.keySym(keyCode,keySym,1);
-        else
-            Log.e("EventsInjectorImpl", "injectKeyPress: Something is wrong. keyCode is " + keyCode + " and keySym is " + keySym);
+    @Override // com.eltechs.axs.xserver.EventsInjector
+    public void injectKeyPress(byte b, int i) {
+
+        this.xServer.getKeyboard().keyPressed(b, i);
     }
 
-    public void injectKeyRelease(byte keyCode, int keySym) {
-        if (keyCode != 0 && keySym == 0)
-            RealXServer.key(keyCode,1);
-        else if (keyCode == 0 && keySym != 0)
-            RealXServer.keySym(keyCode,keySym, 0);
-        else
-            Log.e("EventsInjectorImpl", "injectKeyRelease: Something is wrong. keyCode is " + keyCode + " and keySym is " + keySym);
+    @Override // com.eltechs.axs.xserver.EventsInjector
+    public void injectKeyRelease(byte b, int i) {
+        Log.d(TAG, "injectKeyRelease: 松开按键：keycode="+b+", keysym="+i);
+        this.xServer.getKeyboard().keyReleased(b, i);
     }
 
+    @Override // com.eltechs.axs.xserver.EventsInjector
     public void injectPointerMove(int x, int y) {
-        Log.d(TAG, String.format("injectPointerMove: 移动坐标 (%d,%d)",x,y));
-
-        RealXServer.motion(x, y);
+        this.xServer.getPointer().setCoordinates(x, y);
     }
 
+    @Override // com.eltechs.axs.xserver.EventsInjector
     public void injectPointerButtonPress(int i) {
-        RealXServer.click(i, 1);
+        this.xServer.getPointer().setButton(i, true);
     }
 
+    @Override // com.eltechs.axs.xserver.EventsInjector
     public void injectPointerButtonRelease(int i) {
-        RealXServer.click(i, 0);
+        this.xServer.getPointer().setButton(i, false);
     }
 }
---
+++ app/src/main/java/com/eltechs/axs/xserver/impl/EventsInjectorImpl_new.java
@@ -0,0 +1,48 @@
+//package com.eltechs.axs.xserver.impl;
+//
+//import android.util.Log;
+//
+//import com.eltechs.axs.xserver.EventsInjector;
+//import com.eltechs.axs.xserver.RealXServer;
+//import com.eltechs.axs.xserver.XServer;
+//
+//public class EventsInjectorImpl implements EventsInjector {
+//    private  static final String TAG= "EventsInjectorImpl";
+//    private final XServer xServer;
+//
+//    public EventsInjectorImpl(XServer xServer) {
+//        this.xServer = xServer;
+//    }
+//
+//    public void injectKeyPress(byte keyCode, int keySym) {
+//        if (keyCode != 0 && keySym == 0)
+//            RealXServer.key(keyCode,1);
+//        else if (keyCode == 0 && keySym != 0)
+//            RealXServer.keySym(keyCode,keySym,1);
+//        else
+//            Log.e("EventsInjectorImpl", "injectKeyPress: Something is wrong. keyCode is " + keyCode + " and keySym is " + keySym);
+//    }
+//
+//    public void injectKeyRelease(byte keyCode, int keySym) {
+//        if (keyCode != 0 && keySym == 0)
+//            RealXServer.key(keyCode,1);
+//        else if (keyCode == 0 && keySym != 0)
+//            RealXServer.keySym(keyCode,keySym, 0);
+//        else
+//            Log.e("EventsInjectorImpl", "injectKeyRelease: Something is wrong. keyCode is " + keyCode + " and keySym is " + keySym);
+//    }
+//
+//    public void injectPointerMove(int x, int y) {
+//        Log.d(TAG, String.format("injectPointerMove: 移动坐标 (%d,%d)",x,y));
+//
+//        RealXServer.motion(x, y);
+//    }
+//
+//    public void injectPointerButtonPress(int i) {
+//        RealXServer.click(i, 1);
+//    }
+//
+//    public void injectPointerButtonRelease(int i) {
+//        RealXServer.click(i, 0);
+//    }
+//}
