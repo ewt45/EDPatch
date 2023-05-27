9197808b83ad1e12def905500d8b8955d6c20f54
ewt45 <79033456+ewt45@users.noreply.github.com>
2023-05-27 12:38:33 +0800

    尝试适配termux-x11 居中全屏

--- app/src/main/java/com/example/datainsert/exagear/test/LorieView.java
+++ app/src/main/java/com/example/datainsert/exagear/test/LorieView.java
@@ -6,6 +6,8 @@ import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 
+import java.util.regex.PatternSyntaxException;
+
 public class LorieView extends SurfaceView {
     public LorieView(Context context) {
         super(context);
@@ -26,24 +28,43 @@ public class LorieView extends SurfaceView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         try {
-            SharedPreferences sp = getContext().getSharedPreferences("com.eltechs.ed.CONTAINER_CONFIG_0",Context.MODE_PRIVATE);
-//            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
-            String ratioStr = sp.getString("full_screen_with_ratio","");
-            if(ratioStr!=null && ratioStr.contains(":") ){
-                String[] strings1 = ratioStr.split(":");
-                float ratio = Integer.parseInt(strings1[0])*1f/Integer.parseInt(strings1[1]);
+//            SharedPreferences sp = getContext().getSharedPreferences("com.eltechs.ed.CONTAINER_CONFIG_0", Context.MODE_PRIVATE);
+            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+            boolean shouldResize = sp.getBoolean("should_full_screen_with_ratio", true); //full_screen_with_ratio是字符串类型了
+
+            float ratio; //宽除以高
+            String[] res = null;
+            switch (sp.getString("displayResolutionMode", "native")) {
+                case "exact": {
+                    res = sp.getString("displayResolutionExact", "1280x1024").split("x");
+                    break;
+                }
+                case "custom": {
+                    try {
+                        res = sp.getString("displayResolutionCustom", "1280x1024").split("x");
+                    } catch (NumberFormatException | PatternSyntaxException ignored) {
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            ratio = res != null ? Integer.parseInt(res[0]) * 1.0f / Integer.parseInt(res[1]) : 0f;
+
+            if (ratio != 0 && shouldResize) {
                 int width = MeasureSpec.getSize(widthMeasureSpec);
                 int height = MeasureSpec.getSize(heightMeasureSpec);
                 //左右黑边
-                if(width*1f/height > ratio){
-                    widthMeasureSpec = MeasureSpec.makeMeasureSpec((int) (height*ratio), MeasureSpec.EXACTLY);
+                if (width * 1f / height > ratio) {
+                    widthMeasureSpec = MeasureSpec.makeMeasureSpec((int) (height * ratio), MeasureSpec.EXACTLY);
                 }
                 //上下黑边
-                else{
-                    heightMeasureSpec = MeasureSpec.makeMeasureSpec((int) (width/ratio), MeasureSpec.EXACTLY);
+                else {
+                    heightMeasureSpec = MeasureSpec.makeMeasureSpec((int) (width / ratio), MeasureSpec.EXACTLY);
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
